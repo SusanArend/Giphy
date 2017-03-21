3c6f4b6bf293e4cda90df108ca8c3f3112ac3540
Susan Heiniger <susan.heiniger@gmail.com>
2017-03-21 17:10:53 -0500

    final commit

---
+++ Javascript/giphy.js
@@ -0,0 +1,12 @@
+//Array with animals
+var dogs = ["German Shepard", "Dachshund", "Pug", "Golden Retreiver", "Bulldog"]
+
+
+//Add a fxn to make the buttons and add items to page
+//This will require a for loop
+//AJAX call
+//Query Link
+//Use data attribute
+//add gifs to page
+//Correctly switch from still to animated
+//Include CDN
